283706b2b0cb139a773ca1bc78fa80321fd67577
jay nichols <jay_nichols_3@hotmail.com>
2016-09-21 22:56:28 -0500

    practice with javascript while and do-while loops

---
+++ js/while_loop_practice.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// Counting to 50
+
+var i = 0;
+
+while (i <= 50) {
+    console.log(i);
+    i++;
+}
+
+// Dividing by 5
+
+var g = 250;
+
+while (g > 5) {
+	g = g / 5;
+	console.log(g);
+}
+
+// subtracting 2 each time 
+
+var h = 30;
+
+while (h > 0) {
+	h = h - 2;
+	console.log(h);
+} 
+
+
+// using a do while
+
+var j = 20
+
+do {
+	console.log(j);
+	j--;
+} while (j >= 20);
