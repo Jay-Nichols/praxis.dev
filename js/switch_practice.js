fac020ca6bd794a4042d28344ec7111013290d19
jay nichols <jay_nichols_3@hotmail.com>
2016-09-21 22:34:05 -0500

    practice with switch statements

---
+++ js/switch_practice.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var dinner = "spaghetti";
+
+switch (dinner) {
+	case "lasagna":
+		console.log("I had lasagna for dinner");
+		break;
+	case "pizza":
+		console.log("I had pizza for dinner");
+		break;
+	case "chicken":
+		console.log("I had chicken for dinner");
+		break;
+	case "Whataburger":
+		console.log("I had Whataburger for dinner");
+		break;
+	default:
+		console.log("I had " + dinner + " for dinner.");
+
+
+
+}
