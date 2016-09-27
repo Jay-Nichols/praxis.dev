ddf7ef81687521ddd1dd3b8f55c61182b85af02f
jay nichols <jay_nichols_3@hotmail.com>
2016-09-27 13:17:54 -0500

    animation jquery practice

--- js/morejquery_practice.js
+++ js/morejquery_practice.js
@@ -34,10 +34,40 @@ $(document).ready(function() {
 
 //sliding 
 $(document).ready(function() {
+	$('#gnrSongs').click(function() {
+		$('#gnr').slideToggle();
+	});
+});
+
+$(document).ready(function() {
 	$('#rhcpSongs').click(function() {
 		$('#rhcp').slideToggle();
 	});
 })
 
+//animation
+
+$(document).ready(function() {
+	$('#btn-move').click(function() {
+		$('#box1, #box2').animate({
+			left: "100px"
+		}, 2000);
+	});
+	$('#btn-scale').click(function() {
+		$('#box1, #box2').animate({
+			width: "+5%"
+		}, 500);
+	});
+	$('#btn-hide').click(function() {
+		$('#box1, #box2').animate({
+			opacity: 0
+		}, 3000);
+	});
+	$('#btn-show').click(function() {
+		$('#box1, #box2').animate({
+			opacity: 1
+		}, 5000);
+	});
+});
 
 
